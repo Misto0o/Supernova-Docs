d63b69c034c6ab27eaebc1e92cc1c3602cc784cf
xamatic <rockshowgamer2@gmail.com>
2026-03-24 00:33:10 -0400

    Homepage Finished for tonight

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,15 @@
 // docs/.vitepress/theme/index.ts
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import CustomNavButton from './CustomNavButton.vue'
 
 export default {
-  extends: DefaultTheme
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      // You can add your custom button to the 'nav-bar-content-after' or 'nav-bar-content-before' slot
+      'nav-bar-content-after': () => h(CustomNavButton)
+    })
+  }
 }
